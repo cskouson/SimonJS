4111f1da4032efcd9a55999fb242ded299c88f17
cskouson <angrybuddha23@gmail.com>
2021-06-01 11:37:35 -0700

    click with anim added

--- main.js
+++ main.js
@@ -1,1 +1,28 @@
-alert('hey')
+/**
+ * main.js
+ * Description:
+ * 
+ * @author Clint J Skouson (2021)
+ */
+
+//vars
+let buttonColors = ['red', 'blue', 'green', 'yellow'];
+let randColor = '';
+let gamePattern = [];
+
+//click listener
+$('.btn').on('click', (e) => {
+    $(e.target).fadeOut().fadeIn();
+});
+
+//funcs
+//
+function nextSequence(){
+    let rand = getRand(4);
+    console.log(rand);
+}
+
+//
+function getRand(max){
+    return Math.floor(Math.random()*max);
+}
