4da5416cd471e3444cf4061ae303642662d502e9
cskouson <angrybuddha23@gmail.com>
2021-06-03 12:37:55 -0700

    game logic finished, audio added

--- main.js
+++ main.js
@@ -2,27 +2,72 @@
  * main.js
  * Description:
  * 
- * @author Clint J Skouson (2021)
+ * @author CJS (2021)
  */
 
 //vars
 let buttonColors = ['red', 'blue', 'green', 'yellow'];
-let randColor = '';
 let gamePattern = [];
+let userPattern = [];
+let currentLevel = 0;
+let started = false;
 
 //click listener
 $('.btn').on('click', (e) => {
-    $(e.target).fadeOut().fadeIn();
+    if (started == true) {
+        //fade square
+        $(e.target).fadeOut().fadeIn();
+        let sound = new Audio('sounds/' + e.target.id + '.mp3');
+        sound.volume = 0.25;
+        sound.play();
+        //add to user pattern
+        userPattern.push(e.target.id);
+        //check if most recent user entry matches game pattern at same index, then check if both patterns are same
+        //size to check for pattern match or game over 
+        if (userPattern[userPattern.length - 1] === gamePattern[userPattern.length - 1]) {
+            if (userPattern.length === gamePattern.length) {
+                console.log('round match success');
+                setTimeout(() => { runRound() }, 1300);
+            }
+        } else {
+            console.log('loser...');
+            $('#level-title').html('YOU SUCK! GAME OVERRRRRRR');
+            $('#level-title').after('<h1 id="loser">You made it to level: ' + currentLevel + '</h1>');
+
+            started = false;
+            gamePattern = [];
+            userPattern = [];
+            currentLevel = 0;
+        }
+
+    }
 });
 
-//funcs
-//
-function nextSequence(){
-    let rand = getRand(4);
-    console.log(rand);
+//game start listen
+$('body').on('keypress', (e) => {
+    if (started == false) {
+        $('#loser').remove();
+        if (e.key == 'z') {
+            started = true;
+            runRound();
+        }
+    }
+});
+
+//simulate current round
+function runRound() {
+    currentLevel++;
+    $('#level-title').html('Level: ' + currentLevel);
+    userPattern = [];
+    gamePattern.push(buttonColors[getRand(4)]);
+    console.log('Current game pattern: ' + gamePattern);
+    $('.' + gamePattern[gamePattern.length - 1]).fadeOut(500).fadeIn(500);
+    let sound = new Audio('sounds/' + gamePattern[gamePattern.length - 1] + '.mp3');
+    sound.volume = 0.25;
+    sound.play();
 }
 
-//
-function getRand(max){
-    return Math.floor(Math.random()*max);
+//returns integer bewteen 0 and max
+function getRand(max) {
+    return Math.floor(Math.random() * max);
 }
